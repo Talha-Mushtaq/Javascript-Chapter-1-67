ea4bb2334d7428f206389b5a7d2d8b5e06aa4996
Talha <talhamushtaq6997@gmail.com>
2020-07-23 21:12:07 +0500

    Javascript chapter 1-67
    Javascript chapter 1-67

---
+++ Javascript-Chapter-38-67/App.js
@@ -0,0 +1,122 @@
+// =================== Chapter 38-42 --- Question 1 ===================  
+
+// power(2,3);
+// function power(a,b)
+// {
+//     var result = 1;
+
+//     for(i=1;i<=b;i++)
+//     {    
+//          result = result * a ;
+//     }
+//   document.write("Answer of "+a+" raised to power "+b+" = "+result);
+// }
+
+
+// =================== Chapter 38-42 --- Question 2 ===================  
+
+// var year = + prompt("Enter year to check leap or not");
+
+// (year%4 === 0)
+// ?
+// alert("You entered leap year")
+// :
+// alert("Please enter leap year");
+
+
+// =================== Chapter 38-42 --- Question 3 ===================  
+
+// triangle(2,3,4);
+// function triangle(a,b,c)
+// {
+//     var S = ( a + b + c ) / 2;
+//     var area = S * (S - a) * (S - b) * (S - c);
+
+//  document.write("Area of Triangle = "+area);
+// }
+
+
+// =================== Chapter 38-42 --- Question 4 ===================  
+
+
+// mainFun();
+// function mainFun()
+// {  var m1=20,m2=30,m3=40;
+//     var avg = avgFun(m1,m2,m3);
+//     var per = perFun(m1,m2,m3);
+// document.write("Average = "+avg+" Percentage = "+per);
+// }
+// function avgFun(a,b,c)
+// {
+//     return (a+b+c)/3  ;
+// }
+// function perFun(a,b,c)
+// {
+//     return (a+b+c)*100/300 ;
+// }
+
+
+// =================== Chapter 38-42 --- Question 5 ===================  
+
+
+// var data = prompt("Enter a data","this is data in javascript");
+// var c =  prompt("Enter a string to search");
+// var chk = false;
+// for (i=0;i<data.length;i++)
+// {
+//     var val = c.length;
+
+//     if (data.slice(i,i+val) == c)
+//       {
+//           chk = true;
+//           break;
+//       }
+// }
+
+//     (chk === true)
+//     ?
+//     alert("string found at index of "+i)
+//     :
+//     alert("string not found"); 
+
+
+
+
+// =================== Chapter 38-42 --- Question 6 ===================  
+
+
+
+
+// =================== Chapter 38-42 --- Question 7 ===================  
+
+// function findOccurrences() {
+//     var str = "Pleases read this application and give me gratuity";
+//     var res = str.match(/[aeiou]{2}/g);
+//     return res ? res.length : 0;
+//   }
+  
+//   var found = findOccurrences();
+  
+//   document.write("consective of 2 vowel = " + found);
+
+
+
+// =================== Chapter 38-42 --- Question 8 ===================  
+// =================== Chapter 38-42 --- Question 9 ===================  
+// =================== Chapter 38-42 --- Question 10 ===================  
+
+
+
+
+// =================== Chapter 43-48 --- Question 1,2 ===================  
+
+function myFunction()
+{
+    alert("Thanks for purchase");
+}
+ 
+// =================== Chapter 43-48 --- Question 3 ===================  
+// =================== Chapter 43-48 --- Question 4 ===================  
+// =================== Chapter 43-48 --- Question 5 ===================  
+
+
